ae08cb4faa56c59d9d623d304de9d0a81eb7f93a
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-06-01 11:10:33 -0300

    teste inicial

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`bg-[#102601] h-full`}>
+
+      <View style={tw`text-[#8aa626] flex items-center pt-24`}>
+      {/* Top */}
+
+      <Text style={tw`text-7xl`}>0,00</Text>
+      <Text style={tw`text-4xl`}>PONTOS</Text>
+
+      </View>
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
