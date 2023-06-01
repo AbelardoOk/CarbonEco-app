7ccaa0df0fbf84e6855c0b9d28a7a85f606ee17d
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-06-01 13:24:38 -0300

    texto titulo menu

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
 
-    <View className="">
+    <View className="bg-[#102601] h-full">
 
-    <View className="flex items-center justify-center">
+    <View className="flex items-center">
 
-      <Text className="text-7xl">0,00</Text>
-      <Text className="text-4xl">PONTOS</Text>
+      <Text className="text-5xl font-alice font-regular w-[278px] text-center text-[#B3BF56] pt-24">Olá, como você está?</Text>
+      
       
     </View>
+
+
 
     </View>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      fontFamily: {
+        alice: ['/assets/fonts/Alice.ttf']
+      }
     },
   },
   plugins: [],
